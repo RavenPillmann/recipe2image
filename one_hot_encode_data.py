e549b1422741dbee72b0195379c4ac013e2eacb9
RavenPillmann <rpillmann12@breakthroughsaintpaul.org>
2019-07-01 21:07:38 -0500

    one hot encoding data for the model

--- one_hot_encode_data.py
+++ one_hot_encode_data.py
@@ -3,6 +3,11 @@
 # 2) Next, one hot encode the search term
 # 3) Finally, one hot encode based on the most popular ingredients
 
+import csv
+
+CATEGORIES = ['beef', 'bread', 'breakfast', 'cheese', 'dessert', 'dimsum', 'fish', 'fruit', 'mushroom', 'noodle', 'pasta', 'pizza', 'pork',
+	'potato', 'poultry', 'salad', 'sandwich', 'soup', 'vegetables']
+
 def getNMostCommonIngredients(n):
 	most_common_ingredients = []
 	with open('counts/part-00000', 'r') as count_file:
@@ -17,13 +22,31 @@ def getNMostCommonIngredients(n):
 	return most_common_ingredients
 
 
-def writeRows():
-	# TODO: For each row, place ID, then place one out of however many query terms, finally one-hot encode ingredients
-	pass
+def writeRows(most_common_ingredients):
+	with open('data_one_hot.csv', 'w') as to_file:
+		with open('data.csv', 'r') as from_file:
+			csv_reader = csv.reader(from_file)
+			csv_writer = csv.writer(to_file)
+
+			for row in csv_reader:
+				_id = row[0]
+				category = row[1]
+				one_hot_category = [0 if index != CATEGORIES.index(category) else 1 for index in range(len(CATEGORIES))]
+				
+				ingredients = eval(row[6])
+				one_hot_ingredients = [0 if item not in ingredients else 1 for item in most_common_ingredients]
+
+				write_row = []
+				write_row.append(_id)
+				write_row.extend(one_hot_category)
+				write_row.extend(one_hot_ingredients)
+
+				csv_writer.writerow(write_row)
 
 
 def main():
-	most_common_ingredients = getNMostCommonIngredients(250)	
+	most_common_ingredients = getNMostCommonIngredients(250)
+	writeRows(most_common_ingredients)	
 
 
 if __name__ == "__main__":
